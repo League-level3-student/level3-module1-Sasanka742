4e2b5abe5258583dd361d2a088e2453d53f596d0
Sasanka742 <46304880+Sasanka742@users.noreply.github.com>
2020-08-23 11:26:28 -0700

    integerStackContinued

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -14,28 +14,30 @@ import javax.swing.JTextField;
 //Copyright The League of Amazing Programmers, 2015
 
 public class _06_IPodShuffle implements ActionListener{
+	JFrame frame;
+	JPanel panel;
+	JButton random;
+	JTextField search; 
+	JButton searchLink;
+	ArrayList<String> songAddresses;
+	Song player;
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				JFrame frame = new JFrame("IPod Shuffler");
-				JPanel panel = new JPanel();
-				JButton random = new JButton("Random");
-				JTextField search = new JTextField("Link");
+		
+				frame = new JFrame("IPod Shuffler");
+				panel = new JPanel();
+				random = new JButton("Random");
+				search = new JTextField("");
 				search.setColumns(10);
-				JButton searchLink = new JButton("Play Link");
+				searchLink = new JButton("Play Link");
 				panel.add(searchLink);panel.add(search);panel.add(random);
 				frame.add(panel);frame.setVisible(true);frame.pack();
 				random.addActionListener(this);
 				searchLink.addActionListener(this);
-				
-				Song player = new Song("demo.mp3");
-				//player.play();
-				ArrayList<String> strongAddresses = new ArrayList<String>();
-				strongAddresses.add("https://tif.fjrifj.frl/a4bd09b62495bc98dfb640c4be7114b1/xUNqsfFUwhY/razunzcsuzceo");
-				strongAddresses.add("https://ijl.fjrifj.frl/a42196ca8eaaca081d86064be099e4aa/45cYwDMibGo/rawunwcsuwceo");
-				strongAddresses.add("https://ftl.fjrifj.frl/aa370c876a0e4caa54c3f4e5a4f731bf/NrgmdOz227I/rawunwcsuwceo");
-				strongAddresses.add("https://jjj.fjrifj.frl/1e16227895f3b6751b3f25b726a050cd/QDYfEBY9NM4/raxunxcsuxceo");
-				strongAddresses.add("");
-				
+				player = new Song("demo.mp3");
+				//player song list;
+				songAddresses = new ArrayList<String>();
+				songAddresses.add("demo.mp3");				
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -53,6 +55,17 @@ public class _06_IPodShuffle implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(random.isSelected()) {
+			player.stop();
+			int rand = new Random().nextInt(songAddresses.size());
+			player = new Song(songAddresses.get(rand));
+			player.play();
+		}
+		if(searchLink.isSelected()) {
+			player.stop();
+			String link = search.getText().toString();
+			player = new Song(link);
+			player.play();
+		}
 	}
 }
--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -1,22 +1,29 @@
 package _02_Integer_Stack;
 
+import java.util.Arrays;
+
 public class IntegerStack {
 	//1. create a private array of integers
+	private int[] numbers;
 	
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		numbers = new int[1];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int[] push = new int[numbers.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		push[-1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for(int i=0;i<numbers.length;i++) {
+			push[i] = numbers[i];
+		}
 		//D. set the member array equal to the new array.
+		numbers = push;
 		
 	}
 	
@@ -24,11 +31,14 @@ public class IntegerStack {
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int last = numbers[-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int[] pop = new int[numbers.length-1]
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
+		for(int i=0;i<pop.length;i++) {
+			pop[i] = numbers[i];
+		}
 		
 		//D. set the member array equal to the new array
 		
