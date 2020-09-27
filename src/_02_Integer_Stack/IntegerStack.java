a8e327805a3f190fff9422100c16e606496aaba3
Sasanka742 <46304880+Sasanka742@users.noreply.github.com>
2020-09-27 11:26:16 -0700

    TextUndoStarted

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -9,7 +9,7 @@ public class IntegerStack {
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
-		numbers = new int[1];
+		numbers = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
@@ -17,7 +17,7 @@ public class IntegerStack {
 		//A. create a new array that is one element larger than the member array
 		int[] push = new int[numbers.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		push[-1] = v;
+		push[push.length-1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
 		for(int i=0;i<numbers.length;i++) {
 			push[i] = numbers[i];
@@ -31,9 +31,9 @@ public class IntegerStack {
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		int last = numbers[-1];
+		int last = numbers[numbers.length-1];
 		//B. create a new array that is one element smaller than the member array
-		int[] pop = new int[numbers.length-1]
+		int[] pop = new int[numbers.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
 		for(int i=0;i<pop.length;i++) {
@@ -41,20 +41,20 @@ public class IntegerStack {
 		}
 		
 		//D. set the member array equal to the new array
-		
+		numbers = pop;
 		//E. return the variable you created in step A
-		return 0;
+		return last;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		numbers = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return numbers.length;
 	}
 }
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,13 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +20,33 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	
+	public void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		panel.add(label);
+		frame.add(panel);
+		frame.setVisible(true);
+		label.addKeyListener(this);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("---");
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
