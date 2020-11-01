ff1ae729b712761efe171f4c9276bc6438e726d5
Sasanka742 <46304880+Sasanka742@users.noreply.github.com>
2020-11-01 11:28:03 -0800

    HangManContinue

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -18,13 +18,15 @@ public class HangMan implements KeyListener{
 	 int key = 0;
 	 int counter = 0;
 	 String typed = "";
+	 Stack<String> words;
+	 int health = 3;
 	public void main(String[] args) {
 		 frame = new JFrame("Hangman");
 		 label = new JLabel();
 		 input = new Scanner(System.in);
 		 text = new Utilities();
 		 System.out.println("Enter a number between 1 and 266");
-		Stack<String> words = new Stack<String>();
+		 words = new Stack<String>();
 		int num = input.nextInt(); 
 		
 		for(int i=1;i<num;i++) {
@@ -34,6 +36,7 @@ public class HangMan implements KeyListener{
 			
 		}
 		currentWrd = words.pop();
+		System.out.println("The word: "+currentWrd); //print word to test
 		frame.add(label);
 		blanks = "";
 		for(int i=0;i<currentWrd.length();i++) {
@@ -54,29 +57,51 @@ public class HangMan implements KeyListener{
 		System.out.println("KeyPressed");
 		String pressed = ""+e.getKeyChar();
 		typed = "";
+		if(!currentWrd.contains(pressed)||label.getText().contains(pressed)){
+			health--;
+			//check if game over
+			if(health==0) {
+				System.out.println("Game Over!No more tries");
+				goto newWord;
+			}
+		}	
 		for(int i=0;i<currentWrd.length();i++) {
 			if(currentWrd.charAt(i)==e.getKeyChar()) {
 				if(i==0) {
 					typed = pressed;
 					typed += label.getText().substring(1);
+					label.removeAll();
+					label.setText(typed);
 				}else if(i>0&&i<currentWrd.length()) {
-					
+					typed = label.getText().substring(0,i);
+					typed += pressed;
+					typed += label.getText().substring(i+1);
+					label.removeAll();
+					label.setText(typed);
 				}else if(i==currentWrd.length()-1) {
-					
+					typed = label.getText().substring(0,i-1);
+					typed += pressed;
+					label.removeAll();
+					label.setText(typed);
 				}
-			}
+				//check if word completed
+				if(label.getText().equals(currentWrd)) {
+					newWord:
+						currentWrd = words.pop();
+						blanks = "";
+						for(int k=0;k<currentWrd.length();k++) {
+							blanks+="_";
+						}
+						label.removeAll();
+						label.setText(blanks);
+				}
+			}		
 		}
-		
-			frame.remove(label);
-			label = new JLabel();
-			label.setText(typed);
-			frame.add(label);
-			frame.setVisible(true);
+				
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 	
 }
