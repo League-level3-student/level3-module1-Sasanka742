d142cfd5a1b5210e17030a417ad97c5f9e6adc54
Sasanka742 <46304880+Sasanka742@users.noreply.github.com>
2020-12-13 11:26:38 -0800

    LogSearchContinue

---
+++ src/_06_Intro_To_Hash_Maps/LogSearchTest.java
@@ -0,0 +1,8 @@
+package _06_Intro_To_Hash_Maps;
+
+public class LogSearchTest {
+	public static void main(String[] args) {
+		_02_LogSearch search = new _02_LogSearch();
+		search.run();
+	}
+}
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,12 +1,17 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_LogSearch {
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -35,17 +40,59 @@ public class _02_LogSearch {
 	 *
 	 * */
 	HashMap<Integer,String> names = new HashMap<Integer, String>();
+	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton add = new JButton("Add");
-	JButton id = new JButton("Search by ID");
+	JButton idSearch = new JButton("Search by ID");
 	JButton list = new JButton("Expand List");
 	JButton remove  = new JButton("Delete Entry");
 	
-	public void setup() {
-	panel.add(add);panel.add(id);panel.add(list);panel.add(remove);
-	frame.add(panel);
+	public void run() {
+		//components to panel
+		panel.add(add);panel.add(idSearch);panel.add(list);panel.add(remove);
+		//panel to frame
+		frame.add(panel);
+		//setup frame up
+		frame.setVisible(true);frame.pack();
+		add.addActionListener(this);
+		idSearch.addActionListener(this);
+		list.addActionListener(this);
+		remove.addActionListener(this);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(add)) {
+			String inputID = JOptionPane.showInputDialog("Enter an ID: ");
+			String inputName = JOptionPane.showInputDialog("Enter the name: ");
+			
+			names.put(Integer.parseInt(inputID), inputName);
+			
+		}
+		if(e.getSource().equals(idSearch)) {
+			String inputID = JOptionPane.showInputDialog("Enter an ID: ");
+			
+			if(names.containsKey(Integer.parseInt(inputID))) {
+				JOptionPane.showMessageDialog(null, names.get(Integer.parseInt(inputID)));
+			}
+		}
+		if(e.getSource().equals(list)) {
+			String fullList = "";
+			
+ 			
+ 		
+		}
+		if(e.getSource().equals(remove)) {
+			String inputID = JOptionPane.showInputDialog("Enter an ID: ");
+			
+			if(names.containsKey(Integer.parseInt(inputID))) {
+				names.remove(Integer.parseInt(inputID));
+			}
+		}
+	}
+	
 	
 	
 }
